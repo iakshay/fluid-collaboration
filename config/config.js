e923bcbdf9302babcccfaeed97c02145ec9cb96b
Akshay Aurora <akshayaurora@yahoo.com>
2013-09-16 06:02:01 +0530

    Saving user settings with localStorage

--- config/config.js
+++
@@ -1,5 +0,0 @@
-var config = {};
-config.port = process.env.PORT || 3000;
-config.env = process.env.NODE_ENV || 'development';
-
-module.exports = config;
--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,56 @@
 var fluid_1_5 = fluid_1_5 || {};
 (function ($, fluid) {
+    if(typeof localStorage['userdata'] !== 'undefined' && localStorage['userdata'] !== null){
+      try{
+        FC.settings = JSON.parse(localStorage['userdata']);
+      }catch(e){
+        console.error(e);
+      }
+    }else{
+        FC.settings = {};
+    }
+    
+    $(".resizable").resizable({
+      autoHide: true,
+      handles: 'e',
+      resize: function(e, ui) 
+      {
+          var parent = ui.element.parent();
+          var remainingSpace = parent.width() - ui.element.outerWidth(),
+              divTwo = ui.element.next(),
+              divTwoWidth = (remainingSpace - (divTwo.outerWidth() - divTwo.width()))/parent.width()*100+"%";
+              divTwo.width(divTwoWidth);
+      },
+      stop: function(e, ui) 
+      {
+          var parent = ui.element.parent();
+          ui.element.css(
+          {
+              width: ui.element.width()/parent.width()*100+"%",
+          });
+      }
+    });
+    
+    $('select').change(function(){
+      var optionSelected = $("option:selected", this).val();
+      FC.changeLayout(optionSelected);
+    });
+
+    $('.settings').click(function(){
+      $('.settings-panel').slideToggle();
+      return false;
+    });
+
     FC.init = function(){
-      var that = this;
-      $(".resizable").resizable({
-        autoHide: true,
-        handles: 'e',
-        resize: function(e, ui) 
-        {
-            var parent = ui.element.parent();
-            var remainingSpace = parent.width() - ui.element.outerWidth(),
-                divTwo = ui.element.next(),
-                divTwoWidth = (remainingSpace - (divTwo.outerWidth() - divTwo.width()))/parent.width()*100+"%";
-                divTwo.width(divTwoWidth);
-        },
-        stop: function(e, ui) 
-        {
-            var parent = ui.element.parent();
-            ui.element.css(
-            {
-                width: ui.element.width()/parent.width()*100+"%",
-            });
-        }
-      });
-      
-      $('select').change(function(){
-        var optionSelected = $("option:selected", this).val();
-        that.changeLayout(optionSelected);
-      });
-
-      $('.settings').click(function(){
-        $('.settings-panel').slideToggle();
-        return false;
-      });
-
+      /* Redirect Condition */
+      if(typeof FC.settings.name === 'undefined' || typeof FC.settings.email === 'undefined'){
+        console.log('You should be redirected!');
+        window.open('/#' + window.location.pathname.substring(1), "_self");
+      }else{
+        FC.initRTC();
+      }
+    };
+    FC.initRTC = function(){
       fluid.webrtc('.module-video', {
         signalingServer: FC.signalingServer(),
         room: FC.room,
@@ -81,8 +99,20 @@ var fluid_1_5 = fluid_1_5 || {};
       FC.setWidth($mnotes, mwidths[2]*100 + '%');
     };
 
+
     FC.signalingServer = function(){
       return (window.location.protocol + '//' + window.location.host +'/');
-    }
+    };
 
+    FC.saveDetails = function(name, email, room){
+      FC.settings['name'] = name;
+      FC.settings['email'] = email;
+      FC.settings['room'] = room;
+
+      this.updateDetails();
+    };
+
+    FC.updateDetails = function(){
+      localStorage['userdata'] = JSON.stringify(this.settings);
+    };
 })(jQuery, fluid_1_5);
